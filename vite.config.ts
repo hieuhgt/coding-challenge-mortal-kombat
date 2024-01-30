5e5192cdae71267dc86f35a193cb03864286595c
Richard Allwood <richard.allwood@circlein.com>
2024-01-30 12:58:00 +1000

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+  plugins: [svgr(), react()],
+  test: { environment: 'jsdom' },
+});
